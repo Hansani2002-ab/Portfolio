087f8db968c77db33275aedc52f0c89df3414dc0
Hansani2002-ab <hansanimadurangi21@gmail.com>
2026-02-09 12:00:23 +0530

    Update CV to new version

--- portfolio-website-main/src/contents/projects.ts
+++ portfolio-website-main/src/contents/projects.ts
@@ -70,7 +70,7 @@ export const projects: Project[] = [
       {
         title: 'Simple chatbot',
         description: 'A real-time AI Chatbot built with Python and Flask, powered by OpenRouter API.',
-        technologies: ['Puthon', 'HTML', 'CSS', 'OpenRouter API'],
+        technologies: ['Python', 'HTML', 'CSS', 'OpenRouter API'],
         githubLink: 'https://github.com/Hansani2002-ab/AI-Chatbot',
         image: '/projects/chat.png',
       },
